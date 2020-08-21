fbecf732560df0deecb60c37e9884d4a469fd312
жопа жопа <nelidovn@yandex.ru>
2020-08-21 18:54:28 +0300

    Updates

---
+++ precache-manifest.8da88eaa400dabcd4e45994fe6a0859f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53a2dab9c7a5b92b38041a70dca3eda",
+    "url": "/github-pages-example/index.html"
+  },
+  {
+    "revision": "54d2dbaf10c7239074a5",
+    "url": "/github-pages-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fbbf958749d8f54ffa8",
+    "url": "/github-pages-example/static/js/2.a3d9a700.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-example/static/js/2.a3d9a700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d2dbaf10c7239074a5",
+    "url": "/github-pages-example/static/js/main.82868417.chunk.js"
+  },
+  {
+    "revision": "7dfe86eb2e57922213b3",
+    "url": "/github-pages-example/static/js/runtime-main.34b77bb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
